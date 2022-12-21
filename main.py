23e59badc99e9840bd4555e2aa2869467dde8bbb
MetalManiacMc <93709189+MetalManiacMc@users.noreply.github.com>
2022-12-21 11:35:22 +0100

    Fini dessin des frontières

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ import time
 
 def ktt(coo: list):
     coo[0] -= 160
-    coo[1] = -coo[1]-111
+    coo[1] = -coo[1]+111
+    return coo
+
+
+def ttk(coo: list):
+    coo[0] += 160
+    coo[1] = -coo[1]+111
     return coo
 
 
@@ -25,14 +31,43 @@ def initialize():  # initialise l'écran
 
 
 def draw_borders():
-    for i in range(10):
+    for i in range(20):
+        t.penup()
+        # cherche un point noir d'où commencer le tracé
         x = randrange(0, 321)
         y = randrange(0, 223)
         while k.get_pixel(x, y) != (0, 0, 0):
             x = randrange(0, 321)
             y = randrange(0, 223)
-        print(x, y)
+        #print(x, y, ktt([x, y]))
         t.goto(ktt([x, y]))
+        # prend un angle apporprié à la position
+        if x <= 30:
+            t.setheading(0)
+        elif x >= 190:
+            t.setheading(180)
+        elif y <= 30:
+            t.setheading(270)
+        elif y >= 192:
+            t.setheading(90)
+        # modifie légèrement l'angle
+        t.setheading(t.heading()+randrange(-3, 3))
+        #print(t.position(), ttk(list(t.position())))
+        t.forward(1)
+        t.color("blue")
+        # teste si le pixel devant est noir. S'il ne l'est pas, avance.
+        while k.get_pixel(int(ttk(list(t.position()))[0]), int(ttk(list(t.position()))[1])) != (0, 0, 0):
+            t.pendown()
+            t.backward(1)
+            t.forward(1)
+            t.setheading(float(t.heading())+choice([random(), -random()]))
+            t.penup()
+            t.forward(1)
+        # transforme tout le bleu en noir
+        for L in range(321):
+            for l in range(223):
+                if k.get_pixel(L, l) == (0, 0, 248):
+                    k.set_pixel(L, l, (0, 0, 0))
 
 
 initialize()
