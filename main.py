84a35bbfaac3e72b1d02b2381c334bff9d321779
Vrain308 <121158548+Vrain308@users.noreply.github.com>
2022-12-22 11:12:09 +0100

    Revert "Update main.py"
    This reverts commit 8ce7cb72a6fb2db05655432a41f7906af92a8fd5.

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import time
 colors = [(0, 0, 248)]
 
 
-def ktt(coo: list): #kandinsky to turtle
+def ktt(coo: list):
     coo[0] -= 160
     coo[1] = -coo[1]+111
     return coo
 
 
-def ttk(coo: list): #turtle to kandinsky
+def ttk(coo: list):
     coo[0] += 160
     coo[1] = -coo[1]+111
     return coo
@@ -21,7 +21,6 @@ def ttk(coo: list): #turtle to kandinsky
 def initialize():  # initialise l'écran
     # uniformise le fond d'écran en blanc
     k.fill_rect(0, 0, 320, 222, k.color(248, 252, 248))
-    t.speed(10)
     t.penup()
     # dessine un cadre noir
     t.goto(-160, -111)
@@ -33,33 +32,38 @@ def initialize():  # initialise l'écran
     t.hideturtle()
 
 def draw_borders():
-    t.speed(10)
-    for i in range(50):
+    for i in range(20):
         t.penup()
-        # cherche un pixel d'où commencer le tracé
+        # cherche un point noir d'où commencer le tracé
         x = randrange(0, 321)
         y = randrange(0, 223)
         while k.get_pixel(x, y) != (0, 0, 0):
             x = randrange(0, 321)
             y = randrange(0, 223)
+        #print(x, y, ktt([x, y]))
         t.goto(ktt([x, y]))
-        # prend un angle approprié à la position
-        if x <=160:
-            t.setheading(270)
-        elif x >160:
-            t.setheading(90)
-        if y <=111:
+        # prend un angle apporprié à la position
+        if x <= 30:
             t.setheading(0)
-        elif y >111:
+        elif x >= 190:
             t.setheading(180)
-        # modifie l'angle
-        t.setheading(t.heading()+randrange(-10, 10))
+        elif y <= 30:
+            t.setheading(270)
+        elif y >= 192:
+            t.setheading(90)
+        # modifie légèrement l'angle
+        t.setheading(t.heading()+randrange(-3, 3))
+        #print(t.position(), ttk(list(t.position())))
         t.forward(1)
         t.color("blue")
-        # Si le pixel est blanc, avance.
+        # teste si le pixel devant est noir. S'il ne l'est pas, avance.
         while k.get_pixel(int(ttk(list(t.position()))[0]), int(ttk(list(t.position()))[1])) != (0, 0, 0):
+            t.pendown()
+            t.backward(1)
             t.forward(1)
-            t.right(randrange(-40, 40))
+            t.setheading(float(t.heading())+choice([random(), -random()]))
+            t.penup()
+            t.forward(1)
         # transforme tout le bleu en noir
         for L in range(321):
             for l in range(223):
@@ -68,7 +72,7 @@ def draw_borders():
 
 
 def fill_colors():
-    collist=[color(255,0,0), color(0,255,0), color(0,0,255), color(255,255,0)]
+    pass
 
 initialize()
 draw_borders()
