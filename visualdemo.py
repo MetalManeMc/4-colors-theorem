64658a1a2b0d17e21cbcfd2e8a34dd867bd9d50b
MetalManiacMc <93709189+MetalManiacMc@users.noreply.github.com>
2023-05-11 10:15:14 +0000

    Conventions et retiré erreurs

--- visualdemo.py
+++ visualdemo.py
@@ -1,31 +1,41 @@
-from PIL import Image, ImageDraw
+"""Le module PIL (Pillow) permet de modifier une image."""
 from time import time
 from random import randrange, random, choice
-from time import sleep
+#from time import sleep
+from PIL import Image, ImageDraw
 
 img = Image.new("RGB", (320, 222), "blue")
-imgname = "image_"+str(time())+".png"
+IMGNAME = "image_"+str(time())+".png"
 draw = ImageDraw.Draw(img)
 draw.rectangle((0, 0, 319, 221), "white", "black")
-# img.save(imgname)
+# img.save(IMGNAME)
 
 
-# img.putpixel((x, y), "color")
-# img.getpixel(xy)
+# img.putpixel((coo_x, coo_y), "color")
+# img.getpixel(coo_xy)
 
 class Position():
-    def __init__(self, x=0, y=0, heading: float = 0):
-        self.x = x
-        self.y = y
-        self.coo = (x, y)
+    """
+    Classe enregistrant la position et le comportement d'une sorte de mini-turtle.
+    """
+    def __init__(self, coo_x=0, coo_y=0, heading: float = 0):
+        self.coo_x = coo_x
+        self.coo_y = coo_y
+        self.coo = (coo_x, coo_y)
         self.heading = heading
 
-    def goto(self, x, y):
-        self.x = x
-        self.y = y
-        self.coo = (x, y)
+    def goto(self, coo_x, coo_y):
+        """
+        Déplace l'instance de Position() à une nouvelle coordinnée
+        """
+        self.coo_x = coo_x
+        self.coo_y = coo_y
+        self.coo = (coo_x, coo_y)
 
     def setheading(self, heading):
+        """
+        Règle l'angle par rapport à l'horizontal du mini-turtle
+        """
         if heading < 0:
             heading += 360
         elif heading > 360:
@@ -33,33 +43,47 @@ class Position():
         self.heading = heading
 
     def validate_next(self):
-        NextPosition = Position(self.x, self.y, self.heading)
-        NextPosition.forward()
-        if NextPosition.get() == (255, 255, 255):
-            if not (NextPosition.x > 310 or NextPosition.x < 10 or NextPosition.y > 212 or NextPosition.y < 10):
-                if img.getpixel((NextPosition.x+1, NextPosition.y)) == (0, 0, 0) and img.getpixel((NextPosition.x, NextPosition.y+1)) == (0, 0, 0):
+        """
+        Teste si le position devant est valide pour le tracé des lignes
+        """
+        next_position = Position(self.coo_x, self.coo_y, self.heading)
+        next_position.forward()
+        if next_position.get() == (255, 255, 255):
+            if not (next_position.coo_x > 310 or next_position.coo_x < 10
+                    or next_position.coo_y > 212 or next_position.coo_y < 10):
+                if (img.getpixel((next_position.coo_x+1, next_position.coo_y)) == (0, 0, 0)
+                     and img.getpixel((next_position.coo_x, next_position.coo_y+1)) == (0, 0, 0)):
                     return False
-                if img.getpixel((NextPosition.x-1, NextPosition.y)) == (0, 0, 0) and img.getpixel((NextPosition.x, NextPosition.y+1)) == (0, 0, 0):
+                if (img.getpixel((next_position.coo_x-1, next_position.coo_y)) == (0, 0, 0)
+                     and img.getpixel((next_position.coo_x, next_position.coo_y+1)) == (0, 0, 0)):
                     return False
-                if img.getpixel((NextPosition.x+1, NextPosition.y)) == (0, 0, 0) and img.getpixel((NextPosition.x, NextPosition.y-1)) == (0, 0, 0):
+                if (img.getpixel((next_position.coo_x+1, next_position.coo_y)) == (0, 0, 0)
+                     and img.getpixel((next_position.coo_x, next_position.coo_y-1)) == (0, 0, 0)):
                     return False
-                if img.getpixel((NextPosition.x-1, NextPosition.y)) == (0, 0, 0) and img.getpixel((NextPosition.x, NextPosition.y-1)) == (0, 0, 0):
+                if (img.getpixel((next_position.coo_x-1, next_position.coo_y)) == (0, 0, 0)
+                     and img.getpixel((next_position.coo_x, next_position.coo_y-1)) == (0, 0, 0)):
                     return False
             return True
         return False
 
-    def forward(self, l=1):
-        for i in range(l):
+    def forward(self, dist=1):
+        """
+        Déplace l'instance vers la position devant en fonction de l'angle
+        """
+        for _ in range(dist):
             if self.heading > 292 or self.heading < 68:
-                self.goto(self.x+1, self.y)
+                self.goto(self.coo_x+1, self.coo_y)
             elif self.heading > 112 and self.heading < 248:
-                self.goto(self.x-1, self.y)
+                self.goto(self.coo_x-1, self.coo_y)
             if self.heading > 23 and self.heading < 157:
-                self.goto(self.x, self.y+1)
+                self.goto(self.coo_x, self.coo_y+1)
             elif self.heading > 203 and self.heading < 337:
-                self.goto(self.x, self.y-1)
+                self.goto(self.coo_x, self.coo_y-1)
 
     def place(self, col=(0, 0, 0)):
+        """
+        Place un pixel d'une couleur donnée dur l'image, aux coordonnées du turtule
+        """
         try:
             img.putpixel(self.coo, col)
         except IndexError:
@@ -67,6 +91,9 @@ class Position():
                   str(self.coo) + str(self.heading))
 
     def get(self):
+        """
+        Retourne la couleur du pixel à la position de l'instance
+        """
         try:
             return img.getpixel(self.coo)
         except IndexError:
@@ -77,39 +104,41 @@ pos = Position()
 
 
 def draw_borders():
-    for i in range(20):
-        color = "black"
+    """
+    Dessine les frontières des régions
+    """
+    for _ in range(20):
         # cherche un point noir d'où commencer le tracé
-        x = randrange(0, 320)
-        y = randrange(0, 222)
-        while Position(x, y).get() == (255, 255, 255):
-            x = randrange(0, 320)
-            y = randrange(0, 222)
-        pos.goto(x, y)
+        coo_x = randrange(0, 320)
+        coo_y = randrange(0, 222)
+        while Position(coo_x, coo_y).get() == (255, 255, 255):
+            coo_x = randrange(0, 320)
+            coo_y = randrange(0, 222)
+        pos.goto(coo_x, coo_y)
         # prend un angle apporprié à la position
-        if x <= 10:
+        if coo_x <= 10:
             pos.setheading(0)
-        elif x >= 310:
+        elif coo_x >= 310:
             pos.setheading(180)
-        elif y <= 10:
+        elif coo_y <= 10:
             pos.setheading(90)
-        elif y >= 212:
+        elif coo_y >= 212:
             pos.setheading(270)
         else:
             pos.setheading(randrange(0, 360))
         # modifie "légèrement" l'angle
         pos.setheading(pos.heading+randrange(-30, 30))
-        # print("Initiated line:", pos.x, pos.y, pos.get(), pos.heading)
+        # print("Initiated line:", pos.coo_x, pos.coo_y, pos.get(), pos.heading)
         # teste si le pixel devant est noir. S'il ne l'est pas, avance.
         while pos.validate_next():
-            #print(pos.x, pos.y, pos.get(), pos.heading)
+            #print(pos.coo_x, pos.coo_y, pos.get(), pos.heading)
             pos.forward(1)
             pos.place()
             pos.setheading(pos.heading+choice([10*random(), -10*random()]))
         pos.forward(1)
         pos.place()
-        # print("Killed line:", pos.x, pos.y, pos.get())
+        # print("Killed line:", pos.coo_x, pos.coo_y, pos.get())
 
 
 draw_borders()
-img.save(imgname)
+img.save(IMGNAME)
